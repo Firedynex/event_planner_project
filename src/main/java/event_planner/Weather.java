25427c32d8d3ad3197a31f5c711e95c1dc0c56c9
Firedynex <turjoy10@gmail.com>
2023-06-17 11:07:19 -0400

    Added all the required dependencies

--- src/main/java/event_planner/Weather.java
+++ src/main/java/event_planner/Weather.java
@@ -1,6 +1,13 @@
 package event_planner;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+
+/**
+ * This class is meant to help get the weather forecast for an area.
+ */
 public class Weather {
     
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module event_planner {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires transitive java.net.http;
+    requires transitive com.google.gson;
+
     opens event_planner to javafx.fxml;
     exports event_planner;
 }
