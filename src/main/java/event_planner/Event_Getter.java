47fe292e77939b7ee90e965aa23d676693d3b83c
Firedynex <58797454+Firedynex@users.noreply.github.com>
2023-09-26 18:00:54 -0400

    Fixed curly bracket

--- src/main/java/event_planner/Event_Getter.java
+++ src/main/java/event_planner/Event_Getter.java
@@ -11,20 +11,44 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.nio.charset.StandardCharsets;
-
+import java.util.Properties;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Event_Getter {
-     private static Gson GSON = new GsonBuilder()
-    .setPrettyPrinting()
-    .create();
+
+    private String ninjaKey;
+
+    /** HTTP client. */
+    private static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+        .version(HttpClient.Version.HTTP_2)           // uses HTTP protocol version 2 where possible
+        .followRedirects(HttpClient.Redirect.NORMAL)  // always redirects, except from HTTPS to HTTP
+        .build();                                     // builds and returns a HttpClient object
+
+    /** Google {@code Gson} object for parsing JSON-formatted strings. */
+    private static Gson GSON = new GsonBuilder()
+        .setPrettyPrinting()                          // enable nice output when printing
+        .create();                                    // builds and returns a Gson object
     
-    private static HttpClient HTTP_CLIENT = HttpClient.newBuilder()
-    .version(HttpClient.Version.HTTP_2)
-    .followRedirects(HttpClient.Redirect.NORMAL)
-    .build();
-    
-    
+    /**
+     * Constructor that initializes instance variables.
+     */
+    public Event_Getter() {
+        getKey();
+    }
+
+    /**
+     * Method that gets the API Key.
+     */
+    private void getKey() {
+        try {
+            Properties props = new Properties();
+            BufferedInputStream apiKeyFile = new BufferedInputStream(new FileInputStream("D:\\Coding Projects\\Private_Keys.properties"));
+            props.load(apiKeyFile);
+            ninjaKey = props.getProperty("API_NINJAS_KEY");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
