c84703abbb543c28aaef04fee912f6f81a87f437
Gabe Livengood <gabe@ggliv.com>
2023-10-03 17:37:54 -0400

    format files

--- event-planner/src/main/java/corinth/event_planner/App.java
+++ event-planner/src/main/java/corinth/event_planner/App.java
@@ -2,10 +2,10 @@ package corinth.event_planner;
 
 /**
  * Hello world!
- *
  */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-    }
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
--- event-planner/src/main/java/corinth/event_planner/Country.java
+++ event-planner/src/main/java/corinth/event_planner/Country.java
@@ -4,36 +4,43 @@ package corinth.event_planner;
  * This class represents a country.
  */
 public class Country {
-    /**This is a nested class that represents the currency for the host country. */
-    public class CountryCurrency {
-        private String code;
-        private String name;
 
-        /**
-         * Accessor method for {@code code}.
-         * @return String the value of {@code code}.
-         */
-        public String getCode() {
-            return this.code;
-        }
+  private CountryCurrency currency;
 
-        /**
-         * Accessor method for {@code name}.
-         * @return String the value of {@code name}.
-         */
-        public String getName() {
-            return this.name;
-        }
-    }
+  /**
+   * Returns the value of {@code currency}.
+   *
+   * @return CountryCurrency- the object representation of the currency of a country.
+   */
+  public CountryCurrency getCurrency() {
+    return this.currency;
+  }
 
-    private CountryCurrency currency;
+  /**
+   * This is a nested class that represents the currency for the host country.
+   */
+  public class CountryCurrency {
+
+    private String code;
+    private String name;
 
     /**
-     * Returns the value of {@code currency}.
-     * @return CountryCurrency- the object representation of the currency of a country.
+     * Accessor method for {@code code}.
+     *
+     * @return String the value of {@code code}.
      */
-    public CountryCurrency getCurrency() {
-        return this.currency;
+    public String getCode() {
+      return this.code;
     }
+
+    /**
+     * Accessor method for {@code name}.
+     *
+     * @return String the value of {@code name}.
+     */
+    public String getName() {
+      return this.name;
+    }
+  }
 }
 
