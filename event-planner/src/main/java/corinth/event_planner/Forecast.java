0c08a6d5cfacc5c3a0ccc97e30f3d46e42b80401
Firedynex <58797454+Firedynex@users.noreply.github.com>
2023-12-28 13:43:44 -0500

    Added setter methods for weather nested class

--- event-planner/src/main/java/corinth/event_planner/Forecast.java
+++ event-planner/src/main/java/corinth/event_planner/Forecast.java
@@ -175,6 +175,83 @@ public class Forecast {
             return wind_speed_10m_max;
         }
 
+        /**
+         * Mutator method to set the time.
+         * @param time Day to set the time to.
+         */
+        public void setTime(String time) {
+            this.time = time;
+        }
+
+        /**
+         * Mutator method to set the maximum temperature for the day.
+         * @param temperature_2m_max Temperature to set the maximum temperature to.
+         */
+        public void setTemperature_2m_max(double temperature_2m_max) {
+            this.temperature_2m_max = temperature_2m_max;
+        }
+
+        /**
+         * Mutator method to set the minimum temperature for the day.
+         * @param temperature_2m_min Temperature to set the minimum temperature to.
+         */
+        public void setTemperature_2m_min(double temperature_2m_min) {
+            this.temperature_2m_min = temperature_2m_min;
+        }
+
+        /**
+         * Mutator method to set the sunrise time for the day.
+         * @param sunrise Time to set the sunrise time to.
+         */
+        public void setSunrise(String sunrise) {
+            this.sunrise = sunrise;
+        }
+
+        /**
+         * Mutator method to set the sunset time for the day.
+         * @param sunset Time to set the sunset time to.
+         */
+        public void setSunset(String sunset) {
+            this.sunset = sunset;
+        }
+
+        /**
+         * Mutator method to set the max UV index for the day to.
+         * @param uv_index_max Max UV index to set for the day.
+         */
+        public void setUv_index_max(double uv_index_max) {
+            this.uv_index_max = uv_index_max;
+        }
+
+        /**
+         * Mutator method to set the max UV index for a clear sky to.
+         * @param uv_index_clear_sky_max Max UV index for a clear sky to set.
+         */
+        public void setUv_index_clear_sky_max(double uv_index_clear_sky_max) {
+            this.uv_index_clear_sky_max = uv_index_clear_sky_max;
+        }
+
+        /**
+         * Mutator method to set the maximum precipitation probability of the day to.
+         * @param precipitation_probability_max Value to set the max precipitation probability to.
+         */
+        public void setPrecipitation_probability_max(double precipitation_probability_max) {
+            this.precipitation_probability_max = precipitation_probability_max;
+        }
+
+        /**
+         * Mutator method to set the maximum wind speed to.
+         * @param wind_speed_10m_max Value to set the maximum wind speed for the day to.
+         */
+        public void setWind_speed_10m_max(double wind_speed_10m_max) {
+            this.wind_speed_10m_max = wind_speed_10m_max;
+        }
         
+    }
+
+    private weather[] forecast;
+
+    public Forecast() {
+        this.forecast = new weather[7];
     }
 }
